f73ff4465afce37004060f604a08ea781d152aec
Spuldz <mozgaedvards0@gmail.com>
2022-09-25 22:02:51 +0300

    Completed the smarttoys page

--- frontend/src/pages/SmarttoysPage.tsx
+++ frontend/src/pages/SmarttoysPage.tsx
@@ -3,49 +3,51 @@ import styles from '../css/smarttoys.module.css'
 import threeToys from '../assets/smarttoys/3toys.svg'
 import penguin from '../assets/smarttoys/penguin.svg';
 
-export default function SmarttoysPage(){
-    return(
+export default function SmarttoysPage() {
+    return (
         <>
-            <Header/>
+            <Header />
 
             <div className={styles.topContainer}>
-                <h1 style={{fontSize: "32px"}}>Vai esi gatavs mainīt pasauli?</h1>
-                <p style={{fontSize: "28px"}}>Varoņu rotaļlietas, kas radītas ar īpašo 3D printera tehnoloģiju!</p>
-                <img src={threeToys}/>
+                <h1 style={{ fontSize: "32px" }}>Vai esi gatavs mainīt pasauli?</h1>
+                <p style={{ fontSize: "28px" }}>Varoņu rotaļlietas, kas radītas ar īpašo 3D printera tehnoloģiju!</p>
+                <img src={threeToys} />
             </div>
 
             <div className={styles.middleContainer}>
-             <div className={styles.buttonContainer}>
-             <button className={styles.buttons}>Uzzināt Vairāk</button>
-              <button className={styles.buttons}>Iegādāties</button>
-              <div className={styles.textContainer}>
-                    <p className={styles.eco}>ECO FRIENDLY</p>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.buttons}>Uzzināt Vairāk</button>
+                    <button className={styles.buttons}>Iegādāties</button>
+                    <div className={styles.textContainer}>
+                        <p className={styles.eco}>ECO FRIENDLY</p>
+                    </div>
                 </div>
-             </div>
-            </div> 
+            </div>
 
             <div className={styles.bottomContainer}>
                 <div className={styles.imageContainer}>
-                    <img src={penguin}/>    
-                </div>    
+                    <img src={penguin} />
+                </div>
 
                 <div className={styles.infoContainer}>
                     <div className={styles.boxContainer}>
-                        <div className={styles.boxes} style={{color: "#00D959", fontFamily: "tropical_asian_demoregular, sans-serif"}}>
-                        ECO-FRIENDLY ENGINEERING <br></br> IN IT
+                        <div className={styles.boxes} style={{ color: "#00D959", fontFamily: "tropical_asian_demoregular, sans-serif" }}>
+                            ECO-FRIENDLY ENGINEERING <br></br> IN IT
                         </div>
-                        <div className={styles.boxes} style={{fontSize: "20px", fontWeight: "bold"}}>
+                        <div className={styles.boxes} style={{ fontSize: "20px", fontWeight: "bold" }}>
                             SELF-PAINTED 🎨
                         </div>
                     </div>
 
-                <div className={styles.infoBoxContainer}>
-                    <div className={styles.infoBox}>
-                    Minifigūras bērniem, kas radītas ar 3D tehnoloģijām. Pērkot minifigūru, Jūs iegūstat pieeju platformai bērniem - lietotnei C-APP, kas izmantojot minifigūru un Augmentētās realitātes palīdzību ļauj mācīties dažādus mācību priekšmetus, kā matemātiku, latviešu valodu un angļu valodu. Minifigūras bērniem, kas radītas ar 3D tehnoloģijām. Pērkot minifigūru, Jūs iegūstat pieeju platformai bērniem - lietotnei C-APP, kas izmantojot minifigūru un Augmentētās realitātes palīdzību ļauj mācīties dažādus mācību priekšmetus, kā matemātiku, latviešu valodu un angļu valodu.
+                    <div className={styles.infoBoxContainer}>
+                        <div className={styles.infoBox}>
+                            Minifigūras bērniem, kas radītas ar 3D tehnoloģijām. Pērkot minifigūru, Jūs iegūstat pieeju platformai bērniem - lietotnei C-APP, kas izmantojot minifigūru un Augmentētās realitātes palīdzību ļauj mācīties dažādus mācību priekšmetus, kā matemātiku, latviešu valodu un angļu valodu. Minifigūras bērniem, kas radītas ar 3D tehnoloģijām. Pērkot minifigūru, Jūs iegūstat pieeju platformai bērniem - lietotnei C-APP, kas izmantojot minifigūru un Augmentētās realitātes palīdzību ļauj mācīties dažādus mācību priekšmetus, kā matemātiku, latviešu valodu un angļu valodu.
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>           
+            </div>
+
+        
         </>
     )
 }
